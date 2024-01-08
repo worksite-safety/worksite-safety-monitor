ba4b375de6040fda67b90cfbcce77905765b0e8d
Emre Yılmaz <69767308+Emrey9@users.noreply.github.com>
2024-01-08 22:37:34 +0300

    Demo py fixes

--- designer/src/pages/VideoStream.js
+++ designer/src/pages/VideoStream.js
@@ -7,37 +7,29 @@ const VideoStream = () => {
 
     // Function to update the array
     const updateArray = (newItem) => {
-        // You can set the array using the setMyArray function
         setMyArray((prevArray) => [...prevArray, newItem]);
     };
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            // Append a timestamp to the URL to ensure the image is not cached
             const newImageUrl = `http://localhost:5000/get_image?timestamp=${new Date().getTime()}`;
             updateArray(newImageUrl);
-        }, 100); // Update every 0.1 seconds
+        }, 100);
 
-        return () => clearInterval(intervalId); // Cleanup the interval on component unmount
+        return () => clearInterval(intervalId);
     }, []);
 
     useEffect(() => {
-        // Check if the array length is 100
         if (myArray.length > 15) {
-            // Use an interval to iterate through the image URLs
             let index = 0;
             const intervalId = setInterval(() => {
-                // Update the image URL
                 setImageUrl(myArray[index]);
 
-                // Remove the displayed image from the array
                 setMyArray((prevArray) => prevArray.slice(1));
 
-                // Move to the next image URL
                 index = (index + 1) % myArray.length;
             }, 100); // Update every 0.1 seconds
 
-            // Cleanup the interval on component unmount
             return () => clearInterval(intervalId);
         }
     }, [myArray]);
